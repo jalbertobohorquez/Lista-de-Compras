e53ef18f609ff83f47f3e194d467e5a892d36cbf
jalbertobohorquez <91164244+jalbertobohorquez@users.noreply.github.com>
2022-04-11 01:35:29 -0500

    tarea avance

---
+++ app.js
@@ -0,0 +1,29 @@
+// Variables Globlales
+const formularioUI = document.getElementById('formulario');
+const ListadeComprasUI = document.getElementById('ListadeCompras');
+let arrayListadeCompras = [];
+// Funciones
+const CrearItem = (producto, cantidad) => {
+    let item = {
+        producto: producto,
+        cantidad: cantidad,
+        estado: false
+    }
+    arrayListadeCompras.push(item);
+    return item;
+}
+const GuardarDB = () => {
+    localStorage.setItem('compras', JSON.stringify(arrayListadeCompras));
+}
+//EventListener
+formularioUI.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let productoUI = document.getElementById('producto').value;
+    let cantidadUI = document.getElementById('cantidad').value;
+    console.log(cantidadUI, productoUI);
+
+    CrearItem(productoUI, cantidadUI);
+    GuardarDB();
+    
+    formularioUI.reset();
+});
